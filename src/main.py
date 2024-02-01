724f4b364822fbea613ea7e4f7838bb492c67ff6
xema <xemaieslestacio@gmail.com>
2024-02-01 18:02:11 +0100

    add predict page with react data input

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
 
 # GET next events of a ligue: https://thesportsdb.com/api/v1/json/3/eventsnextleague.php?id=ID_DE_LA_LIGA
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 import requests  # Agrega esta línea para importar la biblioteca requests
 
 app = Flask(__name__)
@@ -26,11 +26,55 @@ def mostrar_liga(name):
   url_leagues = 'https://thesportsdb.com/api/v1/json/3/search_all_teams.php?l='+name
   response_leagues = requests.get(url_leagues)
   data = response_leagues.json() if response_leagues.status_code == 200 else None
-  print(data) 
+  print(url_leagues) 
   return render_template('liga.html', detalles_liga=data)
+
+
+
+@app.route('/liga/<name>/<id_team>')
+def mostrar_resultados_team(name, id_team):
+  url_team_result = 'https://thesportsdb.com/api/v1/json/3/eventslast.php?id='+id_team
+  url_team_next_events = 'https://thesportsdb.com/api/v1/json/3/eventsnext.php?id='+id_team
+  
+  response = requests.get(url_team_result)
+  response_next = requests.get(url_team_next_events)
+  
+  data = response.json() if response.status_code == 200 else None
+  data_next = response_next.json() if response_next.status_code == 200 else None
+  print(data_next)
+  return render_template('team.html', team=data, team_next=data_next)
+
+@app.route('/predict')
+def predict_view():
+  return render_template('predict.html')
+
+@app.route('/api/all_leagues')
+def get_all_leagues():
+  url = 'https://thesportsdb.com/api/v1/json/3/all_leagues.php'
+  response = requests.get(url)
+  
+  if response.status_code == 200:
+    data = response.json()
+    print(data)
+    return jsonify(data)
+  else:
+    return jsonify({'error': 'Error en la solicitud a la API'}), 500
+
+@app.route('/api/teams_by_league')
+def get_all_teams_by_leagues():
+  name = request.args.get('name')
+  url = 'https://thesportsdb.com/api/v1/json/3/search_all_teams.php?l='+name
+  response = requests.get(url)
+  print("URL-> ", url)
+  
+  if response.status_code == 200:
+    data = response.json()
+    print(data)
+    return jsonify(data)
+  else:
+    return jsonify({'error': 'Error en la solicitud a la API'}), 500
+
+    
 
 if __name__ == '__main__':
   app.run(debug=True, port=8000)
-
-
-
---
+++ src/static/predict.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', onLoad);
+
+function onLoad(){
+  console.log("DOM loaded");
+
+  const leagueSelect = document.getElementById('league_select');
+
+  fetch('/api/all_leagues')
+    .then(response => response.json())
+    .then(data => {
+        // Trabaja con los datos recibidos y rellena el select
+        const select = document.getElementById('league_select');
+
+        data.leagues.forEach(league => {
+            const option = document.createElement('option');
+            option.value = league.strLeague;
+            option.text = league.strLeague;
+            select.add(option);
+        });
+        getTeams(leagueSelect.value)
+    })
+    .catch(error => {
+        // Manejar errores
+        console.error('Error en la petición:', error);
+    });
+
+
+
+
+  // Añade un evento 'change' al select
+  leagueSelect.addEventListener('change', function() {
+    getTeams(leagueSelect.value)
+  });
+}
+
+
+function getTeams(name){
+  // Obtén los datos de los equipos
+fetch(`/api/teams_by_league?name=${name}`)
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Error en la solicitud a la API');
+  }
+  return response.json();
+})
+.then(data => {
+  // Obtén las referencias a los elementos select
+  const teamHomeSelect = document.getElementById('team_home_select');
+  const teamVisitorSelect = document.getElementById('team_visitor_select');
+
+  // Limpia cualquier opción existente en los select
+  teamHomeSelect.innerHTML = '';
+  teamVisitorSelect.innerHTML = '';
+
+  // Rellena los select con los nombres de los equipos
+  data.teams.forEach(team => {
+    const optionHome = document.createElement('option');
+    optionHome.value = team.idTeam;
+    optionHome.text = team.strTeam;
+    teamHomeSelect.add(optionHome);
+
+    const optionVisitor = document.createElement('option');
+    optionVisitor.value = team.idTeam;
+    optionVisitor.text = team.strTeam;
+    teamVisitorSelect.add(optionVisitor);
+  });
+})
+.catch(error => {
+  // Manejar errores
+  console.error('Error en la petición:', error);
+});
+}
