eaaad0a7a4b9bed30bdd4d9040b6b851fe34f5db
xema <xemaieslestacio@gmail.com>
2024-02-08 18:01:18 +0100

    add get all events team vs team

--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@
 
 from flask import Flask, render_template, jsonify, request
 import requests  # Agrega esta línea para importar la biblioteca requests
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
 app = Flask(__name__)
 app.config['STATIC_FOLDER'] = 'static'
@@ -30,7 +33,7 @@ def mostrar_liga(name):
   return render_template('liga.html', detalles_liga=data)
 
 
-
+# Devuelve la informacion de los ultimos 5 partidos de un equipo local en un html
 @app.route('/liga/<name>/<id_team>')
 def mostrar_resultados_team(name, id_team):
   url_team_result = 'https://thesportsdb.com/api/v1/json/3/eventslast.php?id='+id_team
@@ -44,10 +47,26 @@ def mostrar_resultados_team(name, id_team):
   print(data_next)
   return render_template('team.html', team=data, team_next=data_next)
 
+# Devuelve los ultimos partidos de un equipo local
+@app.route('/api/get')
+def get_resultados_team():
+  id_team = request.args.get('id_team')
+  url_team_result = 'https://thesportsdb.com/api/v1/json/3/eventslast.php?id='+id_team
+  #url_team_next_events = 'https://thesportsdb.com/api/v1/json/3/eventsnext.php?id='+id_team
+  
+  response = requests.get(url_team_result)
+  #response_next = requests.get(url_team_next_events)
+  
+  data = response.json() if response.status_code == 200 else None
+  #data_next = response_next.json() if response_next.status_code == 200 else None
+  #print(data_next)
+  return jsonify(data)
+
 @app.route('/predict')
 def predict_view():
   return render_template('predict.html')
 
+# Devuelve todas las ligas disponibles
 @app.route('/api/all_leagues')
 def get_all_leagues():
   url = 'https://thesportsdb.com/api/v1/json/3/all_leagues.php'
@@ -60,6 +79,7 @@ def get_all_leagues():
   else:
     return jsonify({'error': 'Error en la solicitud a la API'}), 500
 
+# Devuelve todos los equipos de una liga
 @app.route('/api/teams_by_league')
 def get_all_teams_by_leagues():
   name = request.args.get('name')
@@ -73,6 +93,26 @@ def get_all_teams_by_leagues():
     return jsonify(data)
   else:
     return jsonify({'error': 'Error en la solicitud a la API'}), 500
+
+
+
+
+
+@app.route('/api/get/team_vs_team')
+def get_team_vs_team():
+  id_team_first = request.args.get('id_team_first')
+  id_team_second = request.args.get('id_team_second')
+  url_team_result = 'https://thesportsdb.com/api/v1/json/3/searchevents.php?e=' + id_team_first + "_vs_" + id_team_second
+  print(url_team_result)
+  
+  response = requests.get(url_team_result)
+  
+  data = response.json() if response.status_code == 200 else None
+
+  return jsonify(data)
+
+
+
 
     
 
--- src/static/predict.js
+++ src/static/predict.js
@@ -59,11 +59,13 @@ fetch(`/api/teams_by_league?name=${name}`)
     const optionHome = document.createElement('option');
     optionHome.value = team.idTeam;
     optionHome.text = team.strTeam;
+    optionHome.id = team.strTeam;
     teamHomeSelect.add(optionHome);
 
     const optionVisitor = document.createElement('option');
     optionVisitor.value = team.idTeam;
     optionVisitor.text = team.strTeam;
+    optionVisitor.id = team.strTeam;
     teamVisitorSelect.add(optionVisitor);
   });
 })
@@ -75,10 +77,73 @@ fetch(`/api/teams_by_league?name=${name}`)
 
 
 function onPredict(){
-  let home_team = document.getElementById("team_home_select").value
-  let visitor_team = document.getElementById("team_visitor_select").value
+  let home = document.getElementById("team_home_select");
+  let visitor = document.getElementById("team_visitor_select");
+  
+  let home_team = home.value
+  let visitor_team = visitor.value
+
+  let home_name = home.options[home.selectedIndex].id;
+  let visitor_name = visitor.options[visitor.selectedIndex].id;
+
+
   let date = document.getElementById("date").value;
   let time = document.getElementById("time").value;
 
-  console.log("Predict " + home_team + " vs " + visitor_team + " on " + date + " at " + time);
+  console.log("Predict " + home_name + home_team + " vs " + visitor_name + visitor_team + " on " + date + " at " + time);
+
+  fetch(`/api/get?id_team=${home_team}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error en la solicitud a la API');
+    }
+    return response.json();
+  })
+  .then(data => {
+
+    console.log("----- Last 5 events home playing at home -----")
+    // Rellena los select con los nombres de los equipos
+    data.results.forEach(team => {
+      console.log(team.strHomeTeam + " " + team.intHomeScore + " - " + team.intAwayScore + " " + team.strAwayTeam);  
+    });
+    console.log("----- -----")
+    
+    //console.log(data)
+  })
+  .catch(error => {
+    // Manejar errores
+    console.error('Error en la petición:', error);
+  });
+
+
+
+  console.log(home_name + " vs " + visitor_name)
+  fetch(`/api/get/team_vs_team?id_team_first=${home_name}&id_team_second=${visitor_name}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error en la solicitud a la API');
+    }
+    return response.json();
+  })
+  .then(data => {
+
+    console.log("----- Last events "+home_name+" vs "+visitor_name+" -----")
+    // Rellena los select con los nombres de los equipos
+    data.event.forEach(event => {
+      console.log(event.strHomeTeam + " " + event.intHomeScore + " - " + event.intAwayScore + " " + event.strAwayTeam);
+      
+    });
+    console.log(data)
+    console.log("----- -----")
+  })
+  .catch(error => {
+    // Manejar errores
+    console.error('Error en la petición:', error);
+  });
+
+
+
+
+  // Coger todos los datos posibles de home_team vs visitor_team
+  // En el free solo se puede los ultimos partidos locales, asi que de momento hay que coger los ultimos partidos de home_team
 }
